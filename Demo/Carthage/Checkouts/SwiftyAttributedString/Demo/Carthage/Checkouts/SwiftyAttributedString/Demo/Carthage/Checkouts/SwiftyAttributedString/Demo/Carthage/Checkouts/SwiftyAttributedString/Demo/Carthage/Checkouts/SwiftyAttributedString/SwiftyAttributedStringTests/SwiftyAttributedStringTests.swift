1c4017b4a18f56cba9517372849b915edb7ebb5e
touyu <akkey0222@gmail.com>
2017-05-15 12:35:28 +0900

    v0.4.4

---
+++ Demo/Carthage/Checkouts/SwiftyAttributedString/Demo/Carthage/Checkouts/SwiftyAttributedString/Demo/Carthage/Checkouts/SwiftyAttributedString/Demo/Carthage/Checkouts/SwiftyAttributedString/Demo/Carthage/Checkouts/SwiftyAttributedString/SwiftyAttributedStringTests/SwiftyAttributedStringTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftyAttributedStringTests.swift
+//  SwiftyAttributedStringTests
+//
+//  Created by Yuto Akiba on 2017/05/14.
+//  Copyright © 2017年 Yuto Akiba. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftyAttributedString
+
+class SwiftyAttributedStringTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
